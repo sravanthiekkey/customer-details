49715dc4d1ddadfd5efc9728f6177ee5a4064bfd
Srava <Srava@DESKTOP-MDK0UFT>
2022-02-03 23:53:16 -0800

    customer details

--- 04-02-2022/src/com/demo/example.java
+++ 04-02-2022/src/com/demo/example.java
@@ -6,9 +6,12 @@ public class example {
 		int  customerID=9100;
 		String customername="sravs";
 		String customeraddress="hyd";
+		int customerage=25;
 	    System.out.println(customerID);
 	    System.out.println(customername);
 	    System.out.println(customeraddress);
+	    System.out.println(customerage);
+	    
 
 
 	}
