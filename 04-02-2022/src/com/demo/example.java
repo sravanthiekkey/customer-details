8c70bea58d82b81fa14c8194a9803a8a1ec9f1bc
Srava <Srava@DESKTOP-MDK0UFT>
2022-02-03 23:47:04 -0800

    customer details

---
+++ 04-02-2022/src/com/demo/example.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+public class example {
+
+	public static void main(String[] args) {
+		int  customerID=9100;
+		String customername="sravs";
+		String customeraddress="hyd";
+	    System.out.println(customerID);
+	    System.out.println(customername);
+	    System.out.println(customeraddress);
+
+
+	}
+
+}
